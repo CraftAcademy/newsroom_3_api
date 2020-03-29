791490a46c3200e8e2571223b98cfb833ce6eafe
Philip Gaunitz <fluffen@pgaunitz.se>
2020-03-29 17:55:14 +0200

    stripe test pass

--- spec/requests/api/v1/user_can_purchase_subscription_spec.rb
+++ spec/requests/api/v1/user_can_purchase_subscription_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe 'POST api/v1/subscriptions', type: :request do 
   before do
-    post '/api/v1/subscriptions', params: { stripetoken: '123456', email: 'user@mail.com'}
+    post '/api/v1/subscriptions', params: { stripetoken: 'tok_1GS3I6ETcp1r6Abv4TDXt2Nx', email: 'karlmarx2@mail.com'}
   end
 
   it 'responds with a success status' do
@@ -8,14 +8,14 @@ RSpec.describe 'POST api/v1/subscriptions', type: :request do
   end
 
   it 'creates a customer on Stripe' do
-    expect(Stripe::Customer.list.data.first.email).to eq 'user@mail.com'
+    expect(Stripe::Customer.list.data.first.email).to eq 'karlmarx2@mail.com'
   end
 
   it 'creates a subscriptions for a specific user' do
-    expect(Stripe::Customer.list(email: 'user@mail.com').first.subscriptions).to be_truthy
+    expect(Stripe::Customer.list(email: 'karlmarx2@mail.com').first.subscriptions).to be_truthy
   end
 
   it 'Subscription for specific user is a specific subscription plan' do
-    expect(Stripe::Customer.list(email: 'user@mail.com').first.subscriptions.first.plan.id).to eq 'year_subscription'
+    expect(Stripe::Customer.list(email: 'karlmarx2@mail.com').first.subscriptions.first.plan.id).to eq 'year_subscription'
   end
 end
