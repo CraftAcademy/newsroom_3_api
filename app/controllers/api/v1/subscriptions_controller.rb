c831d7bb644e0955413f311f81dc95920f00616d
Philip Gaunitz <fluffen@pgaunitz.se>
2020-03-29 17:37:36 +0200

    ads more test for stripe, ads logic to controller

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def create
-    customer = Stripe::Customer.retrieve({email: params[:email]})
+    customer = Stripe::Customer.list(email: params[:email]).data.first
     customer = Stripe::Customer.create({email: params[:email], source: params[:stripeToken]}) unless customer
     subscription = Stripe::Subscription.create({customer: customer.id, plan: 'year_subscription'})
     status = Stripe::Invoice.retrieve(subscription.latest_invoice).paid
--- spec/requests/api/v1/user_can_purchase_subscription_spec.rb
+++ spec/requests/api/v1/user_can_purchase_subscription_spec.rb
@@ -7,11 +7,15 @@ RSpec.describe 'POST api/v1/subscriptions', type: :request do
     expect(response_json).to eq JSON.parse({ status: 'paid'}.to_json)
   end
 
+  it 'creates a customer on Stripe' do
+    expect(Stripe::Customer.list.data.first.email).to eq 'user@mail.com'
+  end
+
   it 'creates a subscriptions for a specific user' do
-    # check the entry on stripe to have bla bla as email
+    expect(Stripe::Customer.list(email: 'user@mail.com').first.subscriptions).to be_truthy
   end
 
   it 'Subscription for specific user is a specific subscription plan' do
-    # check the entry on stripe to have bla bla as email
+    expect(Stripe::Customer.list(email: 'user@mail.com').first.subscriptions.first.plan.id).to eq 'year_subscription'
   end
 end
